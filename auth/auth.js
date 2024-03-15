bc31f931cee2938aa9e7ecf9b03b1bf1fb2db774
Ismaël Zerrouk <zerroukism@cy-tech.fr>
2024-03-15 03:16:22 +0100

    add  link with redis database, createUser and login

--- auth/auth.js
+++ auth/auth.js
@@ -1,86 +1,109 @@
-
 const express = require('express');
 const session = require('express-session');
+const redis = require('redis');
+const bodyParser = require('body-parser');
+
+
+
+//const RedisStore = require('connect-redis')(session); // Importez RedisStore
+
+let client = redis.createClient();
+
+(async () => {
+    await client.connect();
+})();
+
+client.on('ready', () => {
+    console.log("Connected to Redis !");
+});
+
+client.on("error", (err) => { 
+    console.log("Error in the Connection"); 
+}); 
+
+
+// Middleware to check if the user is authenticated
+// function isAuthenticated(req, res, next) {
+//     if (req.session && req.session.user) {
+//         return next();
+//     } else {
+//         res.status(401).send('You must be logged in to access this resource');
+//     }
+// }
+
+// module.exports = isAuthenticated;
 
 const app = express();
 app.use(express.static('static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configure express-session middleware
-app.use(session({
-    secret: 'your-secret-key',
-    resave: false,
-    saveUninitialized: true
-}));
+app.get('/', (req, res) => {
+    res.sendFile('views/auth.html', { root: __dirname });
+});
+
 
 // Simulated user database (for demonstration purposes)
-const users = [
-    { username: 'user1', password: 'password1' },
-    { username: 'user2', password: 'password2' }
-];
+// const users = [
+//     { username: 'user1', password: 'password1' },
+//     { username: 'user2', password: 'password2' }
+// ];
 
-// Middleware to parse URL-encoded request bodies
-app.use(express.urlencoded({ extended: true }));
 
 // Define your API routes
-
-// Route to display the registration form
-app.get('/register', (req, res) => {
-    res.send('Please register:<br><form method="post" action="/register">Username: <input type="text" name="username"><br>Password: <input type="password" name="password"><br><input type="submit" value="Register"></form>');
-});
-
-// Handle registration form submission
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-
-    // Check if the username already exists
-    const userExists = users.some(user => user.username === username);
-
-    if (userExists) {
-        // If the username already exists, display an error message
-        res.send('Username already exists. Please choose a different username.');
-    } else {
-        // If the username does not exist, add the new user to the database
-        users.push({ username, password });
-        res.send('Registration successful. You can now login.');
-    }
-});
-
-// Route to handle login page
-app.get('/login', (req, res) => {
-    res.send('Please enter your login and password:<br><form method="post" action="/login">Username: <input type="text" name="username"><br>Password: <input type="password" name="password"><br><input type="submit" value="Login"></form>');
-});
 
 // Handle login form submission
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
+    // Check if the username and password match the ones in the Redis database
+    client.get(`user:${username}`, (_, reply) => {
+        if (reply === password) {
+            req.session.user = username;
+            return res.redirect('http://localhost:3000') // Redirect to the game
+        } else {
+            return res.status(401).send('Invalid username or password');
+        }
+    });
+});
 
-    // Check if the username and password match
-    const user = users.find(user => user.username === username && user.password === password);
+// app.post('/', (req, res) => {
+//     const { username, password } = req.body;
 
-    if (user) {
-        // If correct, save the login in the session
-        req.session.username = username;
-        res.redirect('/game');
+//     // Check if the username and password match
+//     const user = users.find(user => user.username === username && user.password === password);
+
+//     if (user) {
+//         // If correct, save the login in the session
+//         req.session.username = username;
+//         res.redirect('/motus/index.html', { root: __dirname });
+//     } else {
+//         // If not correct, display an error message
+//         res.send('Invalid username or password. Please try again.');
+//     }
+// });
+
+
+app.get('/register', (req, res) => {
+    res.sendFile('views/register.html', { root: __dirname });
+});
+
+// Route to display the registration form
+app.post('/createUser', (req, res) => {
+    const { username, password } = req.body;
+
+    // Store the username and password in the Redis database
+    if (typeof username === 'string' && typeof password === 'string') {
+        client.set(`user:${username}`, password, redis.print);
+        res.sendFile('views/auth.html', { root: __dirname });
     } else {
-        // If not correct, display an error message
-        res.send('Invalid username or password. Please try again.');
+        res.status(400).send('Invalid username or password');
     }
 });
 
-// API endpoint to display game main page
-app.get('/game', (req, res) => {
-    // Check if the user is logged in
-    if (!req.session.username) {
-        // If not logged in, redirect to the login page
-        return res.redirect('/login');
-    }
 
-    // If logged in, display the game main page
-    res.redirect('http://localhost:3000');
-});
 
 // Start the server
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/register`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
